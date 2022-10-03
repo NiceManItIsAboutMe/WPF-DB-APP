8be3a7f217c45f45b1a41ad75cf7d618be6cc77a
NiceManItIsAboutMe <oookosta81@gmail.com>
2022-10-04 01:57:54 +0700

    Добавил метод изменение полей и textbox соответсвенно (но работает криво при нажатии не дает менять текст сам метод выполняется но в поле потом нельзя писать)

--- WpfDBApp/ViewModels/AuthorizationViewModel.cs
+++ WpfDBApp/ViewModels/AuthorizationViewModel.cs
@@ -12,13 +12,28 @@ namespace WpfDBApp.ViewModels
 {
     internal class AuthorizationViewModel : ViewModel
     {
-        // можно вынести команды в отдельный класс, затем только инициализоровать их в конструкторе
+        #region CONST
+        public const string LOGIN = "Введите логин";
+        public const string PASSWORD = "Введите пароль";
+        #endregion
+
+        #region поля в авторизации
+        private string _Login;
+        public string Login
+        {
+            get => _Login;
+            set=>Set(ref _Login, value);
+        }
+        public string Password { get; set; }
+        #endregion
+
+        // можно вынести команды в отдельный класс и вызывать их сразу из view xaml
         #region Команды
         #region Закрытие (тест комманд)
         /// <summary>
         /// объявление команды
         /// </summary>
-		 public ICommand CloseApplicationCommand { get; }
+        public ICommand CloseApplicationCommand { get; }
 
         /// <summary>
         /// условие при котором можно выполнить команду (тут в любом случае выполняем)
@@ -36,23 +51,34 @@ namespace WpfDBApp.ViewModels
         }
 
         #endregion
+
+        #region Изменение текста в TextBox
+        
+        public ICommand ChangeTextBoxTextCommand{get;}
+        private bool CanChangeTextBoxTextCommandExecute(object p) => true;
+        
+        private void OnChangeTextBoxTextCommandExecuted(object p)
+        {
+            if (p.ToString() == LOGIN) { Login = string.Empty; return; }
+            if (p.ToString() == PASSWORD) { Password = string.Empty; return; }
+            if (p.ToString() == string.Empty) { Login = LOGIN; return; }
+            if(p.ToString() == string.Empty) Password = PASSWORD;
+        }
+        #endregion
         #endregion
 
         public AuthorizationViewModel()
         {
+            #region поля
+            Login = LOGIN;
+            Password = PASSWORD;
+            #endregion
             #region команды
             //создание команды
             CloseApplicationCommand = new RelayCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
+
+            ChangeTextBoxTextCommand = new RelayCommand(OnChangeTextBoxTextCommandExecuted, CanChangeTextBoxTextCommandExecute);
             #endregion
         }
-
-        private string login = "login";
-        public string Login
-        {
-            get => login;
-            set => Set(ref login, value);
-        }
-
-        public string Password { get; set; }
     }
 }
