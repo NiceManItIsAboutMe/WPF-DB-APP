6dcd2dd9dbe4287e7685e6c79c0b3626378d93de
NiceManItIsAboutMe <oookosta81@gmail.com>
2022-10-12 04:24:41 +0700

    Добавлено поле Host. Добавлен сервисы для вьюмоделей и для контекста Базы данных.

---
+++ WpfMVVMEfApp/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Windows;
+using WpfMVVMEfApp.Models.PostgreSqlDB;
+using WpfMVVMEfApp.ViewModels;
+using WpfMVVMEfApp.Views;
+
+namespace WpfMVVMEfApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private static IHost _Host;
+
+        public static IHost Host => _Host
+            ??= Program.CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+        public static IServiceProvider Services => Host.Services;
+
+
+        internal static void ConfigureServices(HostBuilderContext host, IServiceCollection services) => services
+            .AddSingleton<MainWindowViewModel>()
+            .AddSingleton<AuthorizationViewModel>()
+            .AddDbContext<ApplicationContext>(opt => opt.UseNpgsql(host.Configuration.GetConnectionString("Bookinist")))
+            ;
+    }
+}
