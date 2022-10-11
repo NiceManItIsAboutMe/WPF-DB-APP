b0623b94232cdd91a3f57f7534e5abbe0f82367d
NiceManItIsAboutMe <oookosta81@gmail.com>
2022-10-12 04:23:11 +0700

    Переопределена точка входа в приложение. Добавлен CreateHostBuilder для EF

---
+++ WpfMVVMEfApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace WpfMVVMEfApp
+{
+    /// <summary>
+    /// точка входа 
+    /// </summary>
+    internal class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            var app=new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+        /// <summary>
+        /// нужен для EF
+        /// </summary>
+        public static IHostBuilder CreateHostBuilder(string[] args) => Host
+            .CreateDefaultBuilder(args)
+            .ConfigureServices(App.ConfigureServices);
+    }
+}
