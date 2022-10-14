40bb9ea30a01189a76476b17a2e7f7d8e181d14c
NiceManItIsAboutMe <oookosta81@gmail.com>
2022-10-15 01:36:19 +0700

    Добавил конвертер, который отображает полное имя наследников класса Person

---
+++ WpfMVVMEfApp/ViewModels/Converters/FullNameConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using WpfMVVMEfApp.Models.Base;
+
+namespace WpfMVVMEfApp.ViewModels.Converters
+{
+    /// <summary>  ///  Конвертирует объект в полное имя  /// </summary>
+    internal class FullNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is Person person)) return value;
+            return person.Surname + " " + person.Name + " " + person.Patronymic;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- WpfMVVMEfApp/ViewModels/MainWindowViewModel.cs
+++ WpfMVVMEfApp/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Windows.Input;
 using WpfDBApp.ViewModels.Base;
+using WpfMVVMEfApp.Commands.Base;
 using WpfMVVMEfApp.Models;
 using WpfMVVMEfApp.Models.PostgreSqlDB;
 
@@ -10,6 +13,8 @@ namespace WpfMVVMEfApp.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
+        #region Поля
+        private ApplicationContext _db;
         #region Заголовок
         private string _Title = "Библиотека";
 
@@ -22,10 +27,28 @@ namespace WpfMVVMEfApp.ViewModels
         private Category _SelectedCategory;
 
         /// <summary> /// Выбранная категория /// </summary>
-        public Category SelectedCategory { get => _SelectedCategory; set => Set(ref _SelectedCategory, value); }
+        public Category SelectedCategory 
+        { get => _SelectedCategory;
+            set
+            {
+                if (AddBooksInCategoriesCommand.CanExecute(value))
+                    AddBooksInCategoriesCommand.Execute(value);
+
+                Set(ref _SelectedCategory, value);
+            }
+        }
 
         #endregion
 
+        #region Выбранный автор
+
+        /// <summary> /// Выбранный автор /// </summary>
+        private Author _SelectedAuthor;
+
+        /// <summary> /// Выбранный автор /// </summary>
+        public Author SelectedAuthor { get => _SelectedAuthor; set => Set(ref _SelectedAuthor, value); }
+
+        #endregion
 
         #region Категории
 
@@ -37,11 +60,50 @@ namespace WpfMVVMEfApp.ViewModels
 
         #endregion
 
+        #region Авторы
 
+        /// <summary> /// Авторы /// </summary>
+        private ObservableCollection<Author> _Authors;
+
+        /// <summary> /// Авторы /// </summary>
+        public ObservableCollection<Author> Authors { get => _Authors; set => Set(ref _Authors, value); }
+
+        #endregion
+
+        #endregion
+
+        #region Команды
+
+        #region команда Добавления книг в категории при выборе категории
+
+        /// <summary> /// Добавления книг в коллекцию при выборе категории /// </summary>
+        public ICommand AddBooksInCategoriesCommand;
+
+        /// <summary> /// Добавления книг в коллекцию при выборе категории /// </summary>
+        public bool CanAddBooksInCategoriesCommandExecute(object? p)
+        {
+            if (p == null) return false;
+            if (((Category) p).Books != null) return false;
+
+            return true;
+        }
+
+        /// <summary> /// Добавления книг в коллекцию при выборе категории /// </summary>
+        public void OnAddBooksInCategoriesCommandExecuted(object? p)
+        {
+            ((Category)p).Books = _db.Categories.Where(c => c.Id == ((Category)p).Id).Select(c => c.Books).FirstOrDefault();
+        }
+
+        #endregion
+
+        #endregion
 
         public MainWindowViewModel(ApplicationContext db)
         {
-            Categories = new ObservableCollection<Category>(db.categories.ToArray());
+            _db = db;
+            Categories = new ObservableCollection<Category>(db.Categories.ToArray());
+            Authors = new ObservableCollection<Author>(db.Authors.ToArray());
+            AddBooksInCategoriesCommand = new RelayCommand(OnAddBooksInCategoriesCommandExecuted, CanAddBooksInCategoriesCommandExecute);
         }
 
     }
