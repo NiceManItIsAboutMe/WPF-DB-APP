5791363c60de735618f0e77d078eb2e5d3e8a48d
NiceManItIsAboutMe <oookosta81@gmail.com>
2022-11-03 16:53:20 +0700

    Add Validator

--- WpfMVVMEfApp/Models/Base/NamedEntity.cs
+++ WpfMVVMEfApp/Models/Base/NamedEntity.cs
@@ -1,10 +1,14 @@
-﻿namespace WpfMVVMEfApp.Models.Base
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WpfMVVMEfApp.Models.Base
 {
     /// <summary>
     /// Именнованая сущность имеет имя
     /// </summary>
     internal abstract class NamedEntity : Entity
     {
+        [Required]
+        [StringLength(100,MinimumLength = 2)]
         public virtual string Name { get; set; }
 
         public override string ToString()
