7295310b4cc2c683edefecc5261d88148041393d
NiceManItIsAboutMe <oookosta81@gmail.com>
2022-11-18 00:36:21 +0700

    refactoring

--- WpfMVVMEfApp/ViewModels/ViewModelLocator.cs
+++ WpfMVVMEfApp/ViewModels/ViewModelLocator.cs
@@ -3,15 +3,6 @@ using WpfMVVMEfApp.ViewModels.AdminViewModels;
 
 namespace WpfMVVMEfApp.ViewModels
 {
-    //Задать в App.xaml
-    /*<Application.Resources>
-        <ResourceDictionary>
-            <vm:ViewModelLocator x:Key="Locator"/>
-        </ResourceDictionary>
-    </Application.Resources>*/
-
-    //вместо локатора можно создать некий контроллер который в конструктор будет принимать все сервисы и хранить их в себе
-    //тоже самое но вместо обращения к App сервисы будут вытакскиваться из конструктора
     internal class ViewModelLocator
     {
 
@@ -20,18 +11,11 @@ namespace WpfMVVMEfApp.ViewModels
 
         public MainWindowViewModel MainWindowModel => App.Services.GetRequiredService<MainWindowViewModel>();
 
-        #region AdminViewModel
         public AdminViewModel AdminModel => App.Services.GetRequiredService<AdminViewModel>();
 
         public UsersViewModel UsersModel => App.Services.GetRequiredService<UsersViewModel>();
         public AuthorsViewModel AuthorsModel => App.Services.GetRequiredService<AuthorsViewModel>();
         public CategoriesViewModel CategoriesModel => App.Services.GetRequiredService<CategoriesViewModel>();
         public BooksViewModel BooksModel => App.Services.GetRequiredService<BooksViewModel>();
-        #endregion
-
-        #region NotAdminViewModel
-
-        #endregion
-
     }
 }
